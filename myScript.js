d8240c75f445f2aac5037f9c0b5dcf15e95334ba
Mohan Kumar <mohankumar007gp@yahoo.com>
2020-08-31 22:29:41 +0530

    all new qns added

--- myScript.js
+++ myScript.js
@@ -1,9 +1,52 @@
-var marksarr =[0,0,0,0,0,0,0,0,0,0];
+//var marksarr =[0,0,0,0,0,0,0,0,0,0];
+result=0;
+
+function start() {
+	var marksarr = {
+		"ten" : "0",
+		"one" : "0",
+		"two" : "0",
+		"three" : "0",
+		"four" : "0",
+		"five" : "0",
+		"six" : "0",
+		"seven" : "0",
+		"eight" : "0",
+		"nine" : "0",
+	}
+	localStorage.setItem("scores",JSON.stringify(marksarr));
+}
 
 function marks(name, value) {
-	name = parseInt(name);
-	value = parseInt(value);
-	marksarr[name] = value;
+	//name = parseInt(name);
+	//value = parseInt(value);
+	var arr = JSON.parse(localStorage.getItem("scores"));
+	arr[name] = value;
 	var x = document.getElementById("asd");
-	x.innerHTML = marksarr;
+	localStorage.setItem("scores",JSON.stringify(arr));
+	x.innerHTML = arr[name];
+}
+
+function finalmarks() {
+	var arr = JSON.parse(localStorage.getItem("scores"));
+	var result = 0;
+	var ansarr = {
+		"ten" : "3",
+		"one" : "4",
+		"two" : "4",
+		"three" : "1",
+		"four" : "3",
+		"five" : "1",
+		"six" : "1",
+		"seven" : "4",
+		"eight" : "4",
+		"nine" : "4",
+	}
+	for(var key in arr) {
+		if (arr[key]==ansarr[key])
+			result +=1;
+	}
+	var x = document.getElementById("asd");
+	// localStorage.setItem("scores",JSON.stringify(arr));
+	x.innerHTML = result;
 }
---
+++ qestionaries.js
@@ -0,0 +1,20 @@
+var questions = [
+    {
+        question: 'Which of the following statement is/are correct about Favipiravir?',
+        options: [
+            'Favipiravir is an antiviral COVID-19 drug.',
+            'Glenmark Pharmaceuticals under the brand name FabiFlu has launched an antiviral drug Favipiravir',
+            ' It is India"s first COVID-19 drug launched, priced at Rs 103 per tablet.',
+            '. All the above are correct'
+        ]
+    },
+    {
+        question: 'How many countries, areas or territories are suffering from novel coronavirus outbreak in the World?',
+        options: [
+            'More than 50',
+            'More than 100',
+            'More than 150',
+            'More than 200'
+        ]
+    }
+];
